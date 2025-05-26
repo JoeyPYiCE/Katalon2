e700abef35dff876a9e70870552641532f8cb172
Joey <Joey@LAPTOP-PTCMI08L>
2025-05-26 11:45:22 +0700

    Test

---
+++ Object Repository/Page_Split PDF files online. Free service t_31c98c/a_Merge PDF.rs
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Merge PDF</name>
+   <tag></tag>
+   <elementGuidId>f2f22668-fbd3-4aa4-bcc3-ca07feeba355</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[contains(text(),'Merge PDF')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>li > a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=link[name=&quot;Merge PDF&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>4f721eee-1ad5-47e2-960a-51bbb4d6ce25</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/merge_pdf</value>
+      <webElementGuid>99ec69aa-5a37-4ce4-b030-d55d4ce94084</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Merge PDF</value>
+      <webElementGuid>4ff8f72d-f021-40cb-bdbf-b3098428c2e1</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[@class=&quot;lang-en-US toolpage&quot;]/header[@class=&quot;header&quot;]/nav[1]/div[@class=&quot;menu&quot;]/ul[1]/li[1]/a[1]</value>
+      <webElementGuid>be1a0a31-23c1-4e88-85a6-3a6b863b5967</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Merge PDF')]</value>
+      <webElementGuid>541ae209-3769-4373-83c6-49850c8a1f3e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='All PDF tools'])[1]/following::a[1]</value>
+      <webElementGuid>02621d40-6445-4ba7-8ddc-3fb807ab5dd2</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Split PDF'])[1]/preceding::a[1]</value>
+      <webElementGuid>e94d81be-43f4-44e3-a91f-124cf0e677f8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Compress PDF'])[1]/preceding::a[2]</value>
+      <webElementGuid>315c7747-ef0f-4305-b011-5ff05da37d64</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Merge PDF']/parent::*</value>
+      <webElementGuid>e6cb9f3a-2e28-4b29-aa66-fd32dd3441dc</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, '/merge_pdf')]</value>
+      <webElementGuid>891f605a-c26f-40d2-9788-b79c493fde3b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//li/a</value>
+      <webElementGuid>ca0a363f-67d8-478e-9fa3-04717ddb4692</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = '/merge_pdf' and (text() = 'Merge PDF' or . = 'Merge PDF')]</value>
+      <webElementGuid>23d066e2-35cb-47cf-b929-f73a8b782ad3</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
